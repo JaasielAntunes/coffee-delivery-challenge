3d08b3d05dea08a003363999192c6568cb289767
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-03-28 16:17:10 -0300

    Página finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { ThemeProvider } from 'styled-components'
+import { Outlet } from 'react-router-dom'
+
 import { Header } from './components/Header'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { CartContextProvider } from './contexts/CartProvider'
 
 export function App() {
   return (
-    <>
-      <ThemeProvider theme={defaultTheme}>
-        <GlobalStyle />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
 
+      <CartContextProvider>
         <Header />
-      </ThemeProvider>
-    </>
+        <Outlet />
+      </CartContextProvider>
+    </ThemeProvider>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,28 @@
-import { MapPin } from 'phosphor-react'
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+import { Link } from 'react-router-dom'
+
+import { useCart } from '../../hooks/useCart'
 import { Aside, Container } from './styles'
 
 export function Header() {
+  const { cart } = useCart()
+
   return (
     <Container>
-      <img src="src/assets/logo.svg" alt="Coffee Delivery" />
+      <Link to="/">
+        <img src="src/assets/logo.svg" alt="Coffee Delivery" />
+      </Link>
 
       <Aside>
         <div>
           <MapPin size={22} weight="fill" />
           <span>João Pessoa, PB</span>
         </div>
+
+        <Link to={`cart`} aria-disabled={cart.length === 0}>
+          <ShoppingCart size={22} weight="fill" />
+          {cart.length > 0 ? <span>{cart.length}</span> : null}
+        </Link>
       </Aside>
     </Container>
   )
