408c936fc0cbda8f58e20567b5216f7af95cd23c
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-03-28 15:33:23 -0300

    Componente Home criado e estilizado

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,71 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import { Heading, Hero, HeroContent, Info } from './styles'
+import { useTheme } from 'styled-components'
+
+export function Home() {
+  const theme = useTheme()
+
+  return (
+    <div>
+      <Hero>
+        <HeroContent>
+          <div>
+            <Heading>
+              <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+              <span>
+                Com o Coffee Delivery você recebe seu café onde estiver, a
+                qualquer hora
+              </span>
+            </Heading>
+
+            <Info>
+              <div>
+                <ShoppingCart
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors['yellow-dark'] }}
+                />
+                <span>Compra simples e segura</span>
+              </div>
+
+              <div>
+                <Package
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors['base-text'] }}
+                />
+                <span>Embalagem mantém o café intacto</span>
+              </div>
+
+              <div>
+                <Timer
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors.yellow }}
+                />
+                <span>Entrega rápida e rastreada</span>
+              </div>
+
+              <div>
+                <Coffee
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors.purple }}
+                />
+                <span>O café chega fresquinho até você</span>
+              </div>
+            </Info>
+          </div>
+
+          <img src="../../assets/hero.svg" alt="Café do Coffee Delivery" />
+        </HeroContent>
+
+        <img src="../../assets/hero-bg.svg" alt="" />
+      </Hero>
+    </div>
+  )
+}
