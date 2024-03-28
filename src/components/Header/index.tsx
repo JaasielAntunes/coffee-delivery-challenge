e371db4c904b59da6dfc35f9b4f9d7e80a6135c3
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-03-28 15:09:03 -0300

    Localização criada e estilizada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
-import { Container } from './styles'
+import { MapPin } from 'phosphor-react'
+import { Aside, Container } from './styles'
 
 export function Header() {
   return (
     <Container>
       <img src="src/assets/logo.svg" alt="Coffee Delivery" />
+
+      <Aside>
+        <div>
+          <MapPin size={22} weight="fill" />
+          <span>João Pessoa, PB</span>
+        </div>
+      </Aside>
     </Container>
   )
 }
