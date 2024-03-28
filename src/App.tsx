39ef5d34735c4cc55a738a161a0f92d06975c914
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-03-28 13:54:37 -0300

    Componente Header criado e estilizado e configuração do styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { Header } from './components/Header'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
-function App() {
+export function App() {
   return (
     <>
-      
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+
+        <Header />
+      </ThemeProvider>
     </>
   )
 }
-
-export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles'
+
+export function Header() {
+  return (
+    <Container>
+      <img src="src/assets/logo.svg" alt="Coffee Delivery" />
+    </Container>
+  )
+}
